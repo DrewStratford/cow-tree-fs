a662128111df232562efda882ac683de2867f311
Drew Stratford <drewstratford@outlook.com>
2024-08-17 11:40:35 +1200

    Remove uneeded test link list.

--- src/file_system.cpp
+++ src/file_system.cpp
@@ -24,55 +24,6 @@ void create_file_system(BufferAllocator& ba, size_t total_pages) {
 	sb->tree_root = initial_root.id();
 }
 
-// Dumb linked list for testing
-struct [[gnu::packed]] FSNode {
-	BlockID next { 0 };
-	size_t size { 0 };
-	char data[PAGE_SIZE - sizeof(BlockID) - sizeof(size)];
-};
-
-void append(BufferAllocator& ba, char* data, size_t len) {
-	auto super_block_raw = ba.load(0);
-	SuperBlock* super_block = (SuperBlock*)super_block_raw.data();
-
-	auto new_page = allocate_page(ba);
-	auto node = (FSNode*)new_page.data();
-	node->next = super_block->tree_root;
-
-	// TODO len check
-	node->size = len;
-	memcpy(node->data, data, len);
-
-	super_block->tree_root = new_page.id();
-
-	super_block_raw.set_dirty();
-	new_page.set_dirty();
-}
-
-void peek(BufferAllocator& ba) {
-	auto super_block_raw = ba.load(0);
-	SuperBlock* super_block = (SuperBlock*)super_block_raw.data();
-
-	auto head = ba.load(super_block->tree_root);
-	auto node = (FSNode*)head.data();
-	printf("peeking: %.*s\n", node->size, node->data);
-}
-
-void pop(BufferAllocator& ba) {
-	auto super_block_raw = ba.load(0);
-	SuperBlock* super_block = (SuperBlock*)super_block_raw.data();
-
-	auto head = ba.load(super_block->tree_root);
-	auto node = (FSNode*)head.data();
-	printf("pop: %.*s\n", node->size, node->data);
-
-	super_block->tree_root = node->next;
-	free_page(ba, head.id());
-
-	super_block_raw.set_dirty();
-	head.set_dirty();
-}
-
 BlockID lookup(BufferAllocator& ba, KeyId key) {
 	auto super_block_raw = ba.load(0);
 	SuperBlock* super_block = (SuperBlock*)super_block_raw.data();
